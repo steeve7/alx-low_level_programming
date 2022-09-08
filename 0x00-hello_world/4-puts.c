e6b0f3cffbc8a768580233e0ce83d34f4b70bb5c
steeve7 <stephenchidindu12@gmail.com>
2022-09-08 14:28:31 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints the sentence
- "Programming is like building a multilingual puzzle"
+ * main prints the sentence "Programming is like building a multilingual puzzle"
  * Return: 0 if corrected, otherwise no 0
  */
 
