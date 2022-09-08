83fa72fc790d19e277ad5190f046009615a4bc8f
steeve7 <stephenchidindu12@gmail.com>
2022-09-08 15:14:46 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints quotes
+ *
+ * Return: zero on sucess
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
