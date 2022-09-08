502ef117ba7d2b5ab1094bdb67d2e2527c44f5af
steeve7 <stephenchidindu12@gmail.com>
2022-09-08 13:47:58 -0700

    puts]

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - prints the phrase "programming is like building a multilingual puzzle"
+ * Return: 0 if corrected, otherwise no 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
