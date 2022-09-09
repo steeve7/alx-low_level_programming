adf6f3784d265926e9ceb6c7eaaec8a5691ea208
steeve7 <stephenchidindu12@gmail.com>
2022-09-09 14:43:40 -0700

    binary letter rev

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Print alphabet
+ * @void: Empty parameter list for main
+ *
+ * Description: Print the alphabet in reverse order
+ * can onlu use putchar
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	char letter = 'z';
+	
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+	return (0);
+}
