41943fd47b6e6bbf02849e2094d8bc355dfb78e8
steeve7 <stephenchidindu12@gmail.com>
2022-09-09 12:37:29 -0700

    summation

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+
 /**
  * main - Entry point
+ *
+ * Description: print the value of n status:
+ * greater than, is zero and is not less than 6.
  *
  * Returns: 0 for success
  */
