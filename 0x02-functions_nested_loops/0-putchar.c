a5b9eb9d4f291afb0635c6176afed966efe1d91a
steeve7 <stephenchidindu12@gmail.com>
2022-09-13 09:32:31 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @void: Empty parameter for main
+ *
+ * Description: Print _putchar followed by a new line
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Learning nested loop
