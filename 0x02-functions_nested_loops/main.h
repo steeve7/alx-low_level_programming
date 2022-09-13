8af802678275175063ae2cc3c263c757417b1f3c
steeve7 <stephenchidindu12@gmail.com>
2022-09-13 11:07:47 -0700

    print putcahr

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -20,10 +20,10 @@
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int_islower(int);
-int_isalpha(int);
+int _islower(int);
+int _isalpha(int);
 int print_sign(int);
-int_abs(int);
+int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
