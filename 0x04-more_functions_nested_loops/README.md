95a2126c9dfa835b01b5f6ae61b5fb710ad9a053
steeve7 <stephenchidindu12@gmail.com>
2022-09-15 12:53:24 -0700

    0-isupper

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+learning more nested loop
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header file for prototype
+ * void protoype (void)
+ * int protoype (int)
+ */
+
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+int _putchar(char);
+int isupper(int);
+int isdigit(int);
+int mul(int, int);
+
+#endif /* MAIN_H */
+	
