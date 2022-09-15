32c09f95e028bc789f21167cdd1943e6a2255de4
steeve7 <stephenchidindu12@gmail.com>
2022-09-15 13:05:45 -0700

    isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,18 +7,30 @@
  * int protoype (int)
  */
 
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
+int _abs(int);
+int print_last_digit(int);
+int add(int, int);
+void jack_bauer(void);
+void times_table(void);
+void print_to_98(int);
+void print_times_table(int);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
-int _putchar(char);
-int isupper(int);
-int isdigit(int);
-int mul(int, int);
+void print_number(int);
+void print_line(int);
+void print_diagonal(int);
+void print_square(int);
+void print_triangle(int);
 
 #endif /* MAIN_H */
 	
